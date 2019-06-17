e70a7cd9b37b960fe2392e07efac75b981aae6c0
edwardrost <edrost.pro@gmail.com>
2019-06-17 14:47:16 +0300

    added filter

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './search-panel.css';
 
+export default class SearchPanel extends Component {
 
-const SearchPanel = () => {
-	const searchText = 'Type here to search';
-	const searchStyle = {
-		fontSize: '20px',
+	state = {
+		term: ''
 	};
 
-	return <input 
-			style = { searchStyle } 
-			placeholder = { searchText }
-			/>;
-};
+	onSearchChange = (e) => {
+		const term = e.target.value;
+		this.setState({ term });
+		this.props.onSearchChange(term);
+	};
 
-export default SearchPanel;
+	render() {
+		return (
+				<input type = "text"
+					   className = "form-control search-input"
+					   placeholder = "type to search" 
+					   value = {this.state.term}
+					   onChange = {this.onSearchChange} />
+			);
+	}
+}
